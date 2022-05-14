f2137e61bcee7412721d38bb960783688a1eff12
Salma Tri Audryani <salmaaudry@gmail.com>
2022-05-14 20:30:46 +0700

    Add change character feature

---
+++ Assets/Scripts/CharaManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaManager : MonoBehaviour
+{
+    public int currentCharaIndex;
+    public GameObject[] characterModels;
+
+    void Start()
+    {
+        currentCharaIndex = PlayerPrefs.GetInt("SelectedChara", 0);
+        foreach(GameObject chara in characterModels)
+        {
+            chara.SetActive(false);
+        }
+        
+        characterModels[currentCharaIndex].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeNext()
+    {
+        characterModels[currentCharaIndex].SetActive(false);
+
+        currentCharaIndex++;
+        if (currentCharaIndex == characterModels.Length)
+        {
+            currentCharaIndex = 0;
+        }
+
+        characterModels[currentCharaIndex].SetActive(true);
+        PlayerPrefs.SetInt("SelectedChara", currentCharaIndex);
+    }
+
+    public void ChangePrev()
+    {
+        characterModels[currentCharaIndex].SetActive(false);
+
+        currentCharaIndex--;
+        if(currentCharaIndex == -1)
+        {
+            currentCharaIndex = characterModels.Length - 1;
+        }
+
+        characterModels[currentCharaIndex].SetActive(true);
+        PlayerPrefs.SetInt("SelectedChara", currentCharaIndex);
+    }
+}
---
+++ Assets/Scripts/CharaSelector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaSelector : MonoBehaviour
+{
+    public int currentCharaIndex;
+    public GameObject[] characters;
+
+    void Start()
+    {
+        currentCharaIndex = PlayerPrefs.GetInt("SelectedChara", 0);
+        foreach(GameObject chara in characters)
+        {
+            chara.SetActive(false);
+        }
+        
+        characters[currentCharaIndex].SetActive(true);
+    }
+}
